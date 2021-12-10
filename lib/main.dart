c8c28583ac0a68a64022a1e94ba8a1d2dcd40129
Susruth <up945169@myport.ac.uk>
2021-12-10 21:04:54 +0000

    Added styling to cards

--- lib/main.dart
+++ lib/main.dart
@@ -27,16 +27,6 @@ class RecipeApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -63,11 +53,27 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   Widget buildRecipeCard(Recipe recipe) {
     return Card(
-      child: Column(
-        children: <Widget>[
-          Image(image: AssetImage(recipe.imageUrl)),
-          Text(recipe.label),
-        ],
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Image(image: AssetImage(recipe.imageUrl)),
+              const SizedBox(
+                height: 14.0,
+              ),
+              Text(
+                recipe.label,
+                style: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'Palatino',
+                ),
+              )
+            ],
+          )
       ),
     );
   }
